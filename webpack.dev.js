d2a0b2d4a9379b566c39845a506ab9a218ca8fd8
Benny <benny@sealights.io>
2017-11-10 09:31:22 +0200

    Package updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 const webpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = webpackMerge(commonConfig, {
 	devServer: {
 		port: 3333
-	}
+	},
+	plugins: [
+		new DashboardPlugin()
+	]
 });
